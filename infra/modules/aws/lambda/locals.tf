7d3ac67295b9afb4186c1c653168488059031f87
chrispsheehan <chrispsheehan@gmail.com>
2025-09-05 14:56:14 +0100

    fix: use correct handler string

--- infra/modules/aws/lambda/locals.tf
+++ infra/modules/aws/lambda/locals.tf
@@ -1,7 +1,7 @@
 locals {
   lambda_runtime = "python3.12"
+  lambda_handler = "lambda_handler"
 
-  lambda_handler      = "${var.lambda_name}.lambda_handler"
   lambda_name         = "${var.environment}-${var.project_name}-${var.lambda_name}"
   lambda_code_zip_key = "${var.lambda_version}/${var.lambda_name}.zip"
 }
