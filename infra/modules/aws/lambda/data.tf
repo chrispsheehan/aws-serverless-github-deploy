c37dcedc2b4f4de0636b5494df73a09c4a2218c4
chrispsheehan <chrispsheehan@gmail.com>
2025-09-05 16:33:40 +0100

    feat: code deploy setup

--- infra/modules/aws/lambda/data.tf
+++ infra/modules/aws/lambda/data.tf
@@ -14,3 +14,40 @@ data "aws_iam_policy_document" "assume_role" {
     actions = ["sts:AssumeRole"]
   }
 }
+
+data "aws_iam_policy_document" "code_deploy_assume" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["codedeploy.amazonaws.com"]
+    }
+  }
+}
+
+data "aws_iam_policy_document" "codedeploy_lambda" {
+  statement {
+    sid    = "LambdaControl"
+    effect = "Allow"
+    actions = [
+      "lambda:GetFunction",
+      "lambda:PublishVersion",
+      "lambda:GetAlias",
+      "lambda:CreateAlias",
+      "lambda:UpdateAlias",
+      "lambda:ListAliases",
+      "lambda:ListVersionsByFunction",
+    ]
+    resources = [
+      aws_lambda_function.fn.arn,
+      "${aws_lambda_function.fn.arn}:*",
+    ]
+  }
+
+  statement {
+    sid       = "DescribeAlarms"
+    effect    = "Allow"
+    actions   = ["cloudwatch:DescribeAlarms"]
+    resources = ["*"]
+  }
+}
--- infra/modules/aws/lambda/main.tf
+++ infra/modules/aws/lambda/main.tf
@@ -11,4 +11,55 @@ resource "aws_lambda_function" "lambda" {
 
   s3_bucket = data.aws_s3_bucket.lambda_code.bucket
   s3_key    = local.lambda_code_zip_key
+
+  # publish ONE immutable version so we can create an alias
+  publish = true
+
+  lifecycle {
+    # Do not update on changes to the initial s3 file version
+    ignore_changes = [
+      s3_bucket,
+      s3_key,
+      s3_object_version,
+    ]
+  }
+}
+
+resource "aws_lambda_alias" "live" {
+  name             = var.environment
+  function_name    = aws_lambda_function.lambda.arn
+  function_version = aws_lambda_function.lambda.version
+
+  # CodeDeploy will repoint this alias → ignore drift
+  lifecycle {
+    ignore_changes = [function_version, routing_config]
+  }
+}
+
+resource "aws_codedeploy_app" "app" {
+  name             = "${local.lambda_name}-app"
+  compute_platform = "Lambda"
+}
+
+resource "aws_iam_role" "cd_role" {
+  name               = "${local.lambda_name}-codedeploy-role"
+  assume_role_policy = data.aws_iam_policy_document.code_deploy_assume.json
+}
+
+resource "aws_iam_role_policy" "cd_lambda" {
+  name   = "${local.lambda_name}-codedeploy-lambda"
+  role   = aws_iam_role.cd_role.id
+  policy = data.aws_iam_policy_document.codedeploy_lambda.json
+}
+
+resource "aws_codedeploy_deployment_group" "dg" {
+  app_name               = aws_codedeploy_app.app.name
+  deployment_group_name  = "${local.lambda_name}-dg"
+  service_role_arn       = aws_iam_role.cd_role.arn
+  deployment_config_name = "CodeDeployDefault.LambdaCanary10Percent5Minutes"
+
+  auto_rollback_configuration {
+    enabled = true
+    events  = ["DEPLOYMENT_FAILURE", "DEPLOYMENT_STOP_ON_ALARM"]
+  }
 }
