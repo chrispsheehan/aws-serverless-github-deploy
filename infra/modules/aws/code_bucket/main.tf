db1cf545ecd8ea2ed1d453521fa2d7285dfa6d3f
Chris Sheehan <chris_sheehan@hotmail.co.uk>
2025-09-06 20:01:16 +0100

    chore: s3 delete old files

--- infra/modules/aws/code_bucket/main.tf
+++ infra/modules/aws/code_bucket/main.tf
@@ -10,3 +10,18 @@ resource "aws_s3_bucket_ownership_controls" "lambda" {
     object_ownership = "BucketOwnerEnforced"
   }
 }
+
+resource "aws_s3_bucket_lifecycle_configuration" "delete_old_files" {
+  count = var.s3_expiration_days > 0 ? 1 : 0
+
+  bucket = aws_s3_bucket.lambda.id
+
+  rule {
+    id     = "delete-expired-objects"
+    status = "Enabled"
+
+    expiration {
+      days = var.s3_expiration_days
+    }
+  }
+}
