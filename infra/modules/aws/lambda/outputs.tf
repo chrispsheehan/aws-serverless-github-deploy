48e78a5edeac09173edbde0c13828ae54c8a13ba
Chris Sheehan <chris_sheehan@hotmail.co.uk>
2025-09-06 20:15:48 +0100

    chore: outputs for code deploy

--- infra/modules/aws/lambda/outputs.tf
+++ infra/modules/aws/lambda/outputs.tf
@@ -9,3 +9,11 @@ output "arn" {
 output "cloudwatch_log_group" {
   value = aws_cloudwatch_log_group.lambda_cloudwatch_group.name
 }
+
+output "code_deploy_app_name" {
+  value = aws_codedeploy_app.app.name
+}
+
+output "code_deploy_group_name" {
+  value = aws_codedeploy_deployment_group.dg.deployment_group_name
+}
