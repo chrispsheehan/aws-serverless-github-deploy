f4a463a95911ecbf34859e732aedbe52e6330483
chrispsheehan <chrispsheehan@gmail.com>
2025-09-08 18:19:44 +0100

    chore: output alias and function name

--- infra/modules/aws/lambda/outputs.tf
+++ infra/modules/aws/lambda/outputs.tf
@@ -6,6 +6,14 @@ output "arn" {
   value = aws_lambda_function.lambda.arn
 }
 
+output "function_name" {
+  value = aws_lambda_function.lambda.function_name
+}
+
+output "alias_name" {
+  value = aws_lambda_alias.live.name
+}
+
 output "cloudwatch_log_group" {
   value = aws_cloudwatch_log_group.lambda_cloudwatch_group.name
 }
