7d335aa6a063f683f2a6ebb6d4ff337505bd9544
chrispsheehan <chrispsheehan@gmail.com>
2026-04-02 14:47:54 +0100

    feat: shared svc module

---
+++ infra/modules/aws/_shared/service/main.tf
@@ -0,0 +1,47 @@
+module "network" {
+  source = "./network"
+
+  count = local.network_count
+
+  vpc_id         = data.aws_vpc.this.id
+  service_name   = var.service_name
+  root_path      = var.root_path
+  container_port = var.container_port
+
+  internal_only = local.internal_only
+
+  api_vpc_link_id           = data.terraform_remote_state.network.outputs.api_id
+  default_target_group_arn  = data.terraform_remote_state.network.outputs.default_target_group_arn
+  default_http_listener_arn = data.terraform_remote_state.network.outputs.default_http_listener_arn
+}
+
+module "ecs" {
+  source = "./ecs"
+
+  security_group_ids = concat(
+    [data.terraform_remote_state.security.outputs.ecs_sg],
+  var.additional_security_group_ids)
+  cluster_id          = data.terraform_remote_state.cluster.outputs.cluster_id
+  service_name        = var.service_name
+  private_subnet_ids  = data.aws_subnets.private.ids
+  container_port      = var.container_port
+  task_definition_arn = var.task_definition_arn
+  desired_task_count  = var.desired_task_count
+
+  xray_enabled = var.xray_enabled
+  local_tunnel = var.local_tunnel
+
+  load_balancers = local.load_balancers
+}
+
+module "auto_scaling" {
+  source = "./auto_scaling"
+
+  cluster_name       = data.terraform_remote_state.cluster.outputs.cluster_name
+  service_name       = var.service_name
+  initial_task_count = var.desired_task_count
+  scaling_strategy   = var.scaling_strategy
+
+  load_balancer_arn_suffix = data.terraform_remote_state.network.outputs.load_balancer_arn_suffix
+  target_group_arn_suffix  = data.terraform_remote_state.network.outputs.target_group_arn_suffix
+}
---
+++ infra/modules/aws/_shared/service/variables.tf
@@ -0,0 +1,98 @@
+variable "state_bucket" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "aws_region" {
+  type = string
+}
+
+variable "vpc_name" {
+  type = string
+}
+
+variable "service_name" {
+  type = string
+}
+
+variable "container_port" {
+  type = number
+}
+
+variable "task_definition_arn" {
+  type = string
+}
+
+variable "root_path" {
+  description = "The path to serve the service from. / is for default /example_service is for subpath"
+  type        = string
+  default     = ""
+}
+
+variable "connection_type" {
+  description = "Type of connectivity/integration to use for the service (choices: internal, internal_dns, vpc_link)."
+  type        = string
+  validation {
+    condition     = can(regex("^(internal|internal_dns|vpc_link)$", var.connection_type))
+    error_message = "connection_type must be one of: internal, internal_dns, vpc_link."
+  }
+}
+
+variable "local_tunnel" {
+  type    = bool
+  default = false
+}
+
+variable "xray_enabled" {
+  type    = bool
+  default = false
+}
+
+variable "wait_for_steady_state" {
+  type    = bool
+  default = false
+}
+
+variable "additional_security_group_ids" {
+  description = "List of security groups to attach to ECS service"
+  type        = list(string)
+  default     = []
+}
+
+variable "desired_task_count" {
+  type = number
+}
+
+variable "scaling_strategy" {
+  type = object({
+    max_scaled_task_count = optional(number)
+    cpu = optional(object({
+      scale_out_threshold  = number
+      scale_in_threshold   = number
+      scale_out_adjustment = number
+      scale_in_adjustment  = number
+      cooldown_out         = number
+      cooldown_in          = number
+    }))
+    sqs = optional(object({
+      scale_out_threshold  = number
+      scale_in_threshold   = number
+      scale_out_adjustment = number
+      scale_in_adjustment  = number
+      cooldown_out         = number
+      cooldown_in          = number
+      queue_name           = string
+    }))
+    alb = optional(object({
+      target_requests_per_task = number
+      cooldown_in              = number
+      cooldown_out             = number
+    }))
+  })
+
+  # {} = "off" by convention
+  default = {}
+}
