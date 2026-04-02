29d33b3331c5bc596c9cf57c583659ad68ac79f8
chrispsheehan <chrispsheehan@gmail.com>
2026-04-02 15:07:09 +0100

    feat: task shared module

---
+++ infra/modules/aws/_shared/task/data.tf
@@ -0,0 +1,81 @@
+data "aws_ecr_repository" "this" {
+  name = var.project_name
+}
+
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+data "aws_iam_policy_document" "logs_policy" {
+  statement {
+    actions = [
+      "logs:CreateLogStream",
+      "logs:PutLogEvents"
+    ]
+
+    effect = "Allow"
+
+    resources = [
+      "${aws_cloudwatch_log_group.ecs_log_group.arn}",
+      "${aws_cloudwatch_log_group.ecs_log_group.arn}:*",
+      "${aws_cloudwatch_log_group.ecs_otel_log_group.arn}",
+      "${aws_cloudwatch_log_group.ecs_otel_log_group.arn}:*"
+    ]
+  }
+}
+
+data "aws_iam_policy_document" "ecr_policy" {
+  statement {
+    actions = [
+      "ecr:GetAuthorizationToken"
+    ]
+    effect    = "Allow"
+    resources = ["*"]
+  }
+
+  statement {
+    actions = [
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:BatchGetImage",
+      "ecr:BatchCheckLayerAvailability"
+    ]
+    effect    = "Allow"
+    resources = [data.aws_ecr_repository.this.arn]
+  }
+}
+
+data "aws_iam_policy_document" "ssm_messages" {
+  statement {
+    actions = [
+      "ssmmessages:CreateControlChannel",
+      "ssmmessages:CreateDataChannel",
+      "ssmmessages:OpenControlChannel",
+      "ssmmessages:OpenDataChannel"
+    ]
+    effect    = "Allow"
+    resources = ["*"]
+  }
+}
+
+data "aws_iam_policy_document" "xray_put" {
+  statement {
+    actions = [
+      "xray:PutTraceSegments",
+      "xray:PutTelemetryRecords",
+      "xray:GetSamplingRules",
+      "xray:GetSamplingTargets",
+      "xray:GetSamplingStatisticSummaries"
+    ]
+    effect    = "Allow"
+    resources = ["*"]
+  }
+}
---
+++ infra/modules/aws/_shared/task/variables.tf
@@ -0,0 +1,73 @@
+variable "project_name" {
+  type = string
+}
+
+variable "service_name" {
+  type = string
+}
+
+variable "aws_region" {
+  type = string
+}
+
+variable "container_port" {
+  type = number
+}
+
+variable "cpu" {
+  type    = number
+  default = 256
+}
+
+variable "memory" {
+  type    = number
+  default = 512
+}
+
+variable "image_uri" {
+  type = string
+}
+
+variable "aws_otel_collector_image_uri" {
+  type = string
+}
+
+variable "otel_sampling_percentage" {
+  description = "Percentage of requests to send to x-ray"
+  type        = string
+  default     = 10.0
+}
+
+variable "debug_image_uri" {
+  type = string
+}
+
+variable "local_tunnel" {
+  type = bool
+}
+
+variable "xray_enabled" {
+  type = bool
+}
+
+variable "additional_env_vars" {
+  type = list(object({
+    name  = string
+    value = string
+  }))
+  default = []
+}
+
+variable "command" {
+  type = list(string)
+}
+
+variable "root_path" {
+  type = string
+}
+
+variable "additional_runtime_policy_arns" {
+  description = "List of IAM runtime policy ARNs to attach to the role"
+  type        = list(string)
+  default     = []
+}
