a3ec19bfa9246d8fc43195dbe3e6163420a7c060
chrispsheehan <chrispsheehan@gmail.com>
2026-04-09 11:35:33 +0100

    fix: name limit issue

--- infra/modules/aws/network/locals.tf
+++ infra/modules/aws/network/locals.tf
@@ -1,4 +1,5 @@
 locals {
-  load_balancer_name = "${var.project_name}-${var.environment}-alb"
-  target_group_name  = "${var.project_name}-${var.environment}-tg"
+  resource_suffix    = substr(md5("${var.project_name}-${var.environment}"), 0, 8)
+  load_balancer_name = "alb-${var.environment}-${local.resource_suffix}"
+  target_group_name  = "tg-${var.environment}-${local.resource_suffix}"
 }
