77c0837dd96348dadd3b4f628a9a0335e2cb7705
chrispsheehan <chrispsheehan@gmail.com>
2026-04-22 16:03:59 +0100

    chore: update oidc module versions

--- infra/modules/aws/_shared/oidc/verisions.tf
+++ infra/modules/aws/_shared/oidc/verisions.tf
@@ -1,8 +1,14 @@
 terraform {
+  required_version = ">= 1.0.8"
+
   required_providers {
+    archive = {
+      source  = "hashicorp/archive"
+      version = "= 2.7.1"
+    }
     aws = {
       source  = "hashicorp/aws"
-      version = "= 4.67.0"
+      version = "= 6.40.0"
     }
   }
 }
